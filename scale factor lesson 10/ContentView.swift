bef0ba31ded02fc1382eeb66ba92803904de9b0e
adina778899 <adinamemetea@yahoo.com>
2025-01-28 11:47:29 -0600

    Initial Commit

---
+++ scale factor lesson 10/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  scale factor lesson 10
+//
+//  Created by natural health on 1/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ scale factor lesson 10/scale_factor_lesson_10App.swift
@@ -0,0 +1,17 @@
+//
+//  scale_factor_lesson_10App.swift
+//  scale factor lesson 10
+//
+//  Created by natural health on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct scale_factor_lesson_10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
