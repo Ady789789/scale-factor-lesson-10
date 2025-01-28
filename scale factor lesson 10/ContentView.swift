5ece03d8ae1937a696147b5e5fd07deb5cfcc516
adina778899 <adinamemetea@yahoo.com>
2025-01-28 12:12:46 -0600

    min scale factors to fit text  into f4rame, basic animation w prof Gallaugher

--- scale factor lesson 10/ContentView.swift
+++ scale factor lesson 10/ContentView.swift
@@ -8,13 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = ""
+    @State private var photoName = ""
+    @State private var photoNumber = 0
+    @State private var messageNumber = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 100)
+                .animation(.easeIn(duration: 0.15), value: message)
+            
+            Image(photoName)
+                .resizable()
+                .scaledToFit()
+                .clipShape(RoundedRectangle(cornerRadius: 45))
+                .shadow(radius: 50)
+                .animation(.default, value: photoName)
+            
+            Spacer()
+            
+            Button("Show Message!") {
+                let messages = ["You are Awesome!",
+                              "You are Great!",
+                              "You are Fantastic!",
+                              "Fabulous? That's You!",
+                                "You  make me Smile!",
+                "When the Genius Bar needs help, they call You!"]
+                message = messages[messageNumber]
+                messageNumber = messageNumber + 1
+                if messageNumber == messages.count {
+                    messageNumber = 0
+                }
+                
+                
+                photoName = "photo\(photoNumber)"
+//                photoNumber = photoNumber + 1
+                photoNumber += 1
+                if photoNumber > 9 {
+                    photoNumber = 0
+                }
+//        print(photoNumber) do not show on content view only in console
+                
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
         }
+        
         .padding()
     }
 }
